211f007f19085928c313d6ace76feb4375647ca9
Urielglb <uriel_06@ciencias.unam.mx>
2024-09-02 19:03:46 -0600

    :recycle: New writter functionality to handle multiple editors

--- src/writter.py
+++ src/writter.py
@@ -1,4 +1,5 @@
 import os
+import hashlib
 
 
 class Writter:
@@ -6,7 +7,7 @@ class Writter:
     The class tha will help us write into files
     """
 
-    def __init__(self, file_name="demo.txt"):
+    def __init__(self, file_name, path):
         """
         Constructor of the class
         ------------------------
@@ -14,18 +15,58 @@ class Writter:
             The name of the file to write
         """
         self.file_name = file_name
+        self.path = path
+        self.original_hash = None
 
-    def write(self, path, content):
-        """
-        The method that will write the content into a file
-        ---------------------------------------------------
-        path: str
-            The path where the file will be written
-        content: str
-            The content to write into the file
-        """
-        if not os.path.exists(path):
-            os.makedirs(path)  # Crea el directorio si no existe
-        full_path = os.path.join(path, self.file_name)
-        with open(full_path, "w") as file:
-            file.write(content)
+    def _compute_hash(self, content):
+        return hashlib.sha256(content.encode("utf-8")).hexdigest()
+
+    def _get_full_path(self):
+        return os.path.join(self.path, self.file_name)
+
+    def open_file(self):
+        if not os.path.exists(self.path):
+            os.makedirs(self.path)
+        path = self._get_full_path()
+        if os.path.exists(path):
+            with open(path, "r") as file:
+                content = file.read()
+            self.original_hash = self._compute_hash(content)
+            return content
+        else:
+            with open(path, "w") as file:
+                file.write("")
+            self.original_hash = self._compute_hash("")
+            return ""
+
+    def save_file(self, new_content):
+        path = self._get_full_path()
+        with open(path, "r") as file:
+            current_content = file.read()
+
+        current_hash = self._compute_hash(current_content)
+
+        if current_hash != self.original_hash:
+            return {
+                "conflict": True,
+                "current_content": current_content,
+                "new_content": new_content,
+            }
+
+        else:
+            with open(path, "w") as file:
+                file.write(new_content)
+            self.original_hash = self._compute_hash(new_content)
+            return {
+                "conflict": False,
+                "message": "El archivo se ha guardado correctamente",
+            }
+
+    def resolve_conflict(self, new_content):
+        path = self._get_full_path()
+        with open(path, "w") as file:
+            file.write(new_content)
+        self.original_hash = self._compute_hash(new_content)
+        return {
+            "message": "El conflicto ha sido resuelto",
+        }
--- tests/test_writter.py
+++ tests/test_writter.py
@@ -5,8 +5,7 @@ from src.writter import Writter
 
 class TestWritter(unittest.TestCase):
     def setUp(self):
-        self.path = "test"
-        self.writter = Writter("test.txt")
+        self.path = "unit-tests"
 
     def tearDown(self):
         demo_path = os.path.join(self.path, "demo.txt")
@@ -16,19 +15,64 @@ class TestWritter(unittest.TestCase):
         if os.path.exists(test_path):
             os.remove(test_path)
 
-    def test_write_default(self):
-        demo_writter = Writter()
-        content = "Hello World! from the demo test"
-        demo_writter.write(self.path, content)
-        expected_path = os.path.join(self.path, "demo.txt")
-        self.assertTrue(os.path.exists(expected_path))
-        with open(expected_path, "r") as file:
-            self.assertEqual(content, file.read())
+    def test_open_file_create_new_file(self):
+        writter = Writter("test.txt", self.path)
+        content = writter.open_file()
+        self.assertEqual(content, "", "El archivo debe estar vacio al ser creado")
+        self.assertTrue(os.path.exists(os.path.join(self.path, "test.txt")))
 
-    def test_write(self):
-        content = "Hello World! from the writter test"
-        self.writter.write(self.path, content)
-        expected_path = os.path.join(self.path, "test.txt")
-        self.assertTrue(os.path.exists(expected_path))
-        with open(expected_path, "r") as file:
-            self.assertEqual(content, file.read())
+    def test_open_file_reads_existing_file(self):
+        test_content = "Contenido de prueba"
+        with open(os.path.join(self.path, "test.txt"), "w") as file:
+            file.write(test_content)
+        writter = Writter("test.txt", self.path)
+        content = writter.open_file()
+        self.assertEqual(
+            content, test_content, "El archivo debe tener el contenido de prueba"
+        )
+
+    def test_write_without_conflict(self):
+        writter = Writter("test.txt", self.path)
+        writter.open_file()
+        new_content = "Nuevo contenido"
+        result = writter.save_file(new_content)
+        self.assertFalse(result["conflict"], "No debe haber conflicto al escribir")
+        self.assertTrue(
+            os.path.exists(os.path.join(self.path, "test.txt")),
+            "El archivo debe existir",
+        )
+        with open(os.path.join(self.path, "test.txt"), "r") as file:
+            saved_content = file.read()
+        self.assertEqual(
+            saved_content, new_content, "El contenido debe ser el nuevo contenido"
+        )
+
+    def test_write_file_with_conflict(self):
+        writter = Writter("test.txt", self.path)
+        original_content = "Contenido original"
+        with open(os.path.join(self.path, "test.txt"), "w") as file:
+            file.write(original_content)
+        writter.open_file()
+        with open(os.path.join(self.path, "test.txt"), "w") as file:
+            file.write("Cambio externo")
+        new_content = "Nuevo contenido"
+        result = writter.save_file(new_content)
+        self.assertTrue(result["conflict"], "Debe haber conflicto al escribir")
+        self.assertEqual(result["current_content"], "Cambio externo")
+        self.assertEqual(result["new_content"], new_content)
+
+    def test_resolve_conflict(self):
+        writter = Writter("test.txt", self.path)
+        original_content = "Contenido original"
+        with open(os.path.join(self.path, "test.txt"), "w") as file:
+            file.write(original_content)
+        writter.open_file()
+        with open(os.path.join(self.path, "test.txt"), "w") as file:
+            file.write("Cambio externo")
+        new_content = "Nuevo contenido"
+        result = writter.save_file(new_content)
+        self.assertTrue(result["conflict"], "Debe haber conflicto al escribir")
+        writter.resolve_conflict(new_content)
+        with open(os.path.join(self.path, "test.txt"), "r") as file:
+            saved_file = file.read()
+        self.assertEquals(saved_file, new_content)
